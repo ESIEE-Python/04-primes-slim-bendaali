771b77dc7af3db2a443afc73d211133225d2ae28
slim-bendaali <slim.bendaali@edu.esiee.fr>
2024-10-15 07:43:19 +0000

    Exercice Prime

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from math import sqrt
+"""
+    Ce fichier comporte la fonction isPrime() et permet de la tester à l'aide du main.
+"""
+
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """
+    Verifie si le nombre est premier.
 
-    # votre code ici
+    Args:
+        Le nombre à verifier
 
-    pass
+    Returns:
+        Un booleen si oui ou non le nombre est premier
+    """
+
+    if p < 2:
+        return False
+    for i in range(2, p):
+        if (p % i) == 0:
+            return False
+
+    return True
+
+
 
 #### Fonction principale
 
 
 def main():
+    """
+        Permet de tester la fonction isPrime().
+    """
 
     # vos appels à la fonction secondaire ici
 
